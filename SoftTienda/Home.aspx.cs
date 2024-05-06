1f0ec4981fa08788aa2a4cef2e9fa20bf6053ce5
Tu Nombre <tu_correo@example.com>
2024-05-06 16:10:25 -0500

    lista_Productos_listo

---
+++ SoftTienda/Home.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SoftTienda
+{
+    public partial class TopBar : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected void handle_listaProductos(object sender, EventArgs e)
+        {
+            Response.Redirect("ListaPedidos.aspx");
+        }
+    }
+}
